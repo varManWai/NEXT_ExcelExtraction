6b823aab81de3101030e32ff053809c9d34585ef
varManWai <laizoke98@gmail.com>
2022-12-25 03:53:40 +0800

    added the file hanlder functon

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,44 @@ import { useState } from "react";
 export default function Home() {
   const [items, setItems] = useState([]);
 
-  
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        console.log(wsname);
+
+        const ws = wb.Sheets[wsname];
+
+        console.log(ws);
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        console.log(data);
+
+        data.map((d) => {
+          setItems([...items, d]);
+        });
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
 
   return (
    
